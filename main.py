da5dd4e5c51bf6f839a1a2ea091672b8cba921c0
lkfd8 <157191141+lkfd8@users.noreply.github.com>
2024-01-20 07:54:05 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import time
 bot_file_name = "vip"
 bot_class_name = "Bot"
 room_id = "659cdef101602e72c92bbd5a"
-bot_token = "afdfb1dc94158e41bc3b1c1103691b0332c6bccd460fc60813776866248c94d6"
+bot_token = "04809b05cb717d3d4051c7cc7750032dd4a902e2956ad775e31ae6d4a7ee4464"
 
 
 my_bot = BotDefinition(getattr(import_module(bot_file_name), bot_class_name)(), room_id, bot_token)
